5ea489dd63b5a881fa710773cd776e73d08f74e3
vladimirB <vladimirzq@gmail.com>
2021-01-23 03:02:29 +0600

    Updates

---
+++ precache-manifest.3075347511d6fe31d713fceb73355c1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534b1d3cc878b516d38f83281a01ff16",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "8a96fa0d06e3c9920370",
+    "url": "/todo-list-react/static/css/main.22f66140.chunk.css"
+  },
+  {
+    "revision": "23e6e7645b196a6f3c6a",
+    "url": "/todo-list-react/static/js/2.9f835767.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todo-list-react/static/js/2.9f835767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a96fa0d06e3c9920370",
+    "url": "/todo-list-react/static/js/main.ddc4c2b6.chunk.js"
+  },
+  {
+    "revision": "76c267ac4b8026bcc596",
+    "url": "/todo-list-react/static/js/runtime-main.fb8cb754.js"
+  }
+]);
